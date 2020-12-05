a4a547ab448f185b47cdd350e290160b02cf25e8
Victor Enaud <7222886+victorenaud@users.noreply.github.com>
2020-12-05 10:11:10 +0100

    Day 3: Part 2

--- src/main/java/Map.kt
+++ src/main/java/Map.kt
@@ -1,17 +1,17 @@
-class Map(private val mapLines: List<String>) {
+class Map(val rightMovement: Int, val downMovement: Int, private val mapLines: List<String>) {
     private val position = Position()
     private val treeSymbol = '#'
 
     fun numberOfTreesInTobogganPath(): Int {
         var numberOfTreesEncountered = 0
         while (position.currentLine < mapLines.size - 1) {
-            position.move()
+            position.move(rightMovement, downMovement)
             if (isTreeOnPosition()) numberOfTreesEncountered++
         }
         return numberOfTreesEncountered
     }
 
-    fun isTreeOnPosition(): Boolean {
+    private fun isTreeOnPosition(): Boolean {
         val numberOfColumns = mapLines[0].count()
         return mapLines[position.currentLine][position.currentColumn % numberOfColumns] == treeSymbol
     }
@@ -20,9 +20,9 @@ class Map(private val mapLines: List<String>) {
         internal var currentLine = 0
         internal var currentColumn = 0
 
-        fun move() {
-            currentLine++
-            currentColumn += 3
+        fun move(rightMovement: Int, downMovement: Int) {
+            currentLine+= downMovement
+            currentColumn += rightMovement
         }
     }
 }
--- src/test/java/MapTest.kt
+++ src/test/java/MapTest.kt
@@ -8,7 +8,7 @@ internal class MapTest {
     @Test
     fun `should not encounter tree on path when there is none`() {
         // Given
-        val map = Map(listOf("..##.......", "#...#...#.."))
+        val map = Map(3, 1, listOf("..##.......", "#...#...#.."))
 
         // When
         val numberOfTreesEncountered = map.numberOfTreesInTobogganPath()
@@ -20,7 +20,7 @@ internal class MapTest {
     @Test
     fun `should encounter one tree on next move when there is one`() {
         // Given
-        val map = Map(listOf("..##.......", "#...#...#..", ".#....#..#."))
+        val map = Map(3, 1, listOf("..##.......", "#...#...#..", ".#....#..#."))
 
         // When
         val numberOfTreesEncountered = map.numberOfTreesInTobogganPath()
@@ -32,11 +32,15 @@ internal class MapTest {
     @Test
     fun `should encounter two trees on a larger map`() {
         // Given
-        val map = Map(listOf("..##.........",
+        val map = Map(
+            3, 1, listOf(
+                "..##.........",
                 "#...#...#..#.",
                 ".#....#..#..#",
                 "..#.#...#.#..",
-                ".....##..#..#"))
+                ".....##..#..#"
+            )
+        )
 
         // When
         val numberOfTreesEncountered = map.numberOfTreesInTobogganPath()
@@ -46,9 +50,11 @@ internal class MapTest {
     }
 
     @Test
-    fun `should encounter seven trees on test map`() {
+    fun `should encounter two trees on test map with a movement of right 1 down 1`() {
         // Given
-        val map = Map(listOf("..##.......",
+        val map = Map(
+            1, 1, listOf(
+                "..##.......",
                 "#...#...#..",
                 ".#....#..#.",
                 "..#.#...#.#",
@@ -58,7 +64,113 @@ internal class MapTest {
                 ".#........#",
                 "#.##...#...",
                 "#...##....#",
-                ".#..#...#.#"))
+                ".#..#...#.#"
+            )
+        )
+
+        // When
+        val numberOfTreesEncountered = map.numberOfTreesInTobogganPath()
+
+        // Then
+        assertThat(numberOfTreesEncountered).isEqualTo(2)
+    }
+
+    @Test
+    fun `should encounter three trees on test map with a movement of right 5 down 1`() {
+        // Given
+        val map = Map(
+            5, 1, listOf(
+                "..##.......",
+                "#...#...#..",
+                ".#....#..#.",
+                "..#.#...#.#",
+                ".#...##..#.",
+                "..#.##.....",
+                ".#.#.#....#",
+                ".#........#",
+                "#.##...#...",
+                "#...##....#",
+                ".#..#...#.#"
+            )
+        )
+
+        // When
+        val numberOfTreesEncountered = map.numberOfTreesInTobogganPath()
+
+        // Then
+        assertThat(numberOfTreesEncountered).isEqualTo(3)
+    }
+
+    @Test
+    fun `should encounter four trees on test map with a movement of right 7 down 1`() {
+        // Given
+        val map = Map(
+            7, 1, listOf(
+                "..##.......",
+                "#...#...#..",
+                ".#....#..#.",
+                "..#.#...#.#",
+                ".#...##..#.",
+                "..#.##.....",
+                ".#.#.#....#",
+                ".#........#",
+                "#.##...#...",
+                "#...##....#",
+                ".#..#...#.#"
+            )
+        )
+
+        // When
+        val numberOfTreesEncountered = map.numberOfTreesInTobogganPath()
+
+        // Then
+        assertThat(numberOfTreesEncountered).isEqualTo(4)
+    }
+
+    @Test
+    fun `should encounter two trees on test map with a movement of right 1 down 2`() {
+        // Given
+        val map = Map(
+            1, 2, listOf(
+                "..##.......",
+                "#...#...#..",
+                ".#....#..#.",
+                "..#.#...#.#",
+                ".#...##..#.",
+                "..#.##.....",
+                ".#.#.#....#",
+                ".#........#",
+                "#.##...#...",
+                "#...##....#",
+                ".#..#...#.#"
+            )
+        )
+
+        // When
+        val numberOfTreesEncountered = map.numberOfTreesInTobogganPath()
+
+        // Then
+        assertThat(numberOfTreesEncountered).isEqualTo(2)
+    }
+
+    @Test
+    fun `should encounter seven trees on test map with a movement of right 1 down 3`() {
+        // Given
+        val map = Map(
+            3, 1, listOf(
+                "..##.......",
+                "#...#...#..",
+                ".#....#..#.",
+                "..#.#...#.#",
+                ".#...##..#.",
+                "..#.##.....",
+                ".#.#.#....#",
+                ".#........#",
+                "#.##...#...",
+                "#...##....#",
+                ".#..#...#.#"
+            )
+        )
 
         // When
         val numberOfTreesEncountered = map.numberOfTreesInTobogganPath()
@@ -70,12 +182,34 @@ internal class MapTest {
     @Test
     fun `day 3 - part 1`() {
         // Given
-        val map = Map(REAL_MAP)
+        val map = Map(3, 1, REAL_MAP)
 
         // When
         val numberOfTreesEncountered = map.numberOfTreesInTobogganPath()
 
         // Then
         assertThat(numberOfTreesEncountered).isEqualTo(254)
+    }
+
+    @Test
+    fun `day 3 - part 2`() {
+        // Given
+        val right1Down1Map = Map(1, 1, REAL_MAP)
+        val right3Down1Map = Map(3, 1, REAL_MAP)
+        val right5Down1Map = Map(5, 1, REAL_MAP)
+        val right7Down1Map = Map(7, 1, REAL_MAP)
+        val right1Down2Map = Map(1, 2, REAL_MAP)
+        val right1Down1NumberOfTreesEncountered = right1Down1Map.numberOfTreesInTobogganPath()
+        val right3Down1NumberOfTreesEncountered = right3Down1Map.numberOfTreesInTobogganPath()
+        val right5Down1NumberOfTreesEncountered = right5Down1Map.numberOfTreesInTobogganPath()
+        val right7Down1NumberOfTreesEncountered = right7Down1Map.numberOfTreesInTobogganPath()
+        val right1Down2NumberOfTreesEncountered = right1Down2Map.numberOfTreesInTobogganPath()
+
+        // When
+        val treesMultiplication =
+            right1Down1NumberOfTreesEncountered * right3Down1NumberOfTreesEncountered * right5Down1NumberOfTreesEncountered * right7Down1NumberOfTreesEncountered * right1Down2NumberOfTreesEncountered
+
+        // Then
+        assertThat(treesMultiplication).isEqualTo(1666768320)
     }
 }
