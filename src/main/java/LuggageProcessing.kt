c544d06f9af28b833f54a38ed9d6bcbe81458e1a
Victor Enaud <7222886+victorenaud@users.noreply.github.com>
2020-12-11 19:34:08 +0100

    Day 7: Part 2

--- src/main/java/LuggageProcessing.kt
+++ src/main/java/LuggageProcessing.kt
@@ -2,16 +2,16 @@ class LuggageProcessing(textLuggageRules: List<String>) {
 
     private val luggageRules = textLuggageRules.map { LuggageRule(it) }
     private val luggageRulesTree = luggageRules.map { parentBag ->
-        LuggageRuleNode(parentBag.color, buildChildNodes(parentBag))
+        LuggageRuleNode(parentBag.color, 1, buildChildNodes(parentBag))
     }
 
-    private fun buildChildNodes(parentBag: LuggageRule):List<LuggageRuleNode> =
+    private fun buildChildNodes(parentBag: LuggageRule): List<LuggageRuleNode> =
         parentBag.containedColors.map { containedColor ->
-            val containedColorRule = luggageRules.find { luggageRule -> luggageRule.color == containedColor }
+            val containedColorRule = luggageRules.find { luggageRule -> luggageRule.color == containedColor.color }
             if (containedColorRule != null) {
-                LuggageRuleNode(containedColorRule.color, buildChildNodes(containedColorRule))
+                LuggageRuleNode(containedColor.color, containedColor.number, buildChildNodes(containedColorRule))
             } else {
-                LuggageRuleNode(containedColor, emptyList())
+                LuggageRuleNode(containedColor.color, containedColor.number, emptyList())
             }
         }
 
@@ -20,23 +20,43 @@ class LuggageProcessing(textLuggageRules: List<String>) {
         return luggageRulesTree.filter { it.containColor(bagColor) }.count()
     }
 
+    fun numberOfBagsContainedBy(bagColor: String): Int {
+        return luggageRulesTree.find { it.isColor(bagColor) }?.numberOfContainedBags() ?: 0
+    }
+
     inner class LuggageRule(textLuggageRule: String) {
         private val bagSuffixRegex = " bag.*".toRegex()
         val color = textLuggageRule.split(" contain ")[0].replace(bagSuffixRegex, "")
         val containedColors = textLuggageRule.split(" contain ")[1]
             .split(", ")
-            .map {
-                it.replace(bagSuffixRegex, "")
-                    .drop(2)
-            }
+            .filter { !it.contains("no other bags") }
+            .map { ContainedColor(it.replace(bagSuffixRegex, "").drop(2), it.take(1)) }
+
+        inner class ContainedColor(val color: String, textNumber: String) {
+            val number = textNumber.toInt()
+        }
     }
 
-    inner class LuggageRuleNode(val color: String, val containedLuggages: List<LuggageRuleNode?>) {
+    inner class LuggageRuleNode(
+        private val color: String,
+        private val number: Int,
+        private val containedLuggages: List<LuggageRuleNode?>
+    ) {
+        fun numberOfContainedBags(): Int {
+            return if (containedLuggages.isEmpty()) {
+                0
+            } else {
+                containedLuggages.fold(0) { numberOfContainedBags, containedLuggage ->
+                    numberOfContainedBags + containedLuggage!!.number + containedLuggage.number * containedLuggage.numberOfContainedBags()
+                }
+            }
+        }
+
         fun containColor(bagColor: String): Boolean {
             return containedLuggages.any { it?.isColor(bagColor) ?: false || it?.containColor(bagColor) ?: false }
         }
 
-        private fun isColor(bagColor: String): Boolean {
+        fun isColor(bagColor: String): Boolean {
             return color == bagColor
         }
     }
--- src/test/java/LuggageProcessingTest.kt
+++ src/test/java/LuggageProcessingTest.kt
@@ -1,70 +1,174 @@
 import org.assertj.core.api.Assertions.assertThat
+import org.junit.jupiter.api.Nested
 import org.junit.jupiter.api.Test
 import java.io.File
 
 val REAL_LUGGAGE_RULES = File("src/test/resources/DAY_7_DATA.txt").readLines()
 
 internal class LuggageProcessingTest {
-    @Test
-    fun `should return 0 when no bags can contain given bag color`() {
-        // Given
-        val luggageProcessing = LuggageProcessing(listOf("light red bags contain no other bags."))
+    @Nested
+    inner class NumberOfBagsContainingColor {
+        @Test
+        fun `should return 0 when no bags can contain given bag color`() {
+            // Given
+            val luggageProcessing = LuggageProcessing(listOf("light red bags contain no other bags."))
 
-        // When
-        val numberOfBagsContainingColor = luggageProcessing.numberOfBagsContainingColor("shiny gold")
+            // When
+            val numberOfBagsContainingColor = luggageProcessing.numberOfBagsContainingColor("shiny gold")
 
-        // Then
-        assertThat(numberOfBagsContainingColor).isEqualTo(0)
+            // Then
+            assertThat(numberOfBagsContainingColor).isEqualTo(0)
+        }
+
+        @Test
+        fun `should return 1 when one bag can contain given bag color`() {
+            // Given
+            val luggageProcessing = LuggageProcessing(listOf("bright white bags contain 1 shiny gold bag."))
+
+            // When
+            val numberOfBagsContainingColor = luggageProcessing.numberOfBagsContainingColor("shiny gold")
+
+            // Then
+            assertThat(numberOfBagsContainingColor).isEqualTo(1)
+        }
+
+        @Test
+        fun `should return 2 when a bag contains a bag that can contain given bag color`() {
+            // Given
+            val luggageProcessing = LuggageProcessing(
+                listOf(
+                    "bright white bags contain 1 shiny gold bag.",
+                    "dark orange bags contain 3 bright white bags, 4 muted yellow bags."
+                )
+            )
+
+            // When
+            val numberOfBagsContainingColor = luggageProcessing.numberOfBagsContainingColor("shiny gold")
+
+            // Then
+            assertThat(numberOfBagsContainingColor).isEqualTo(2)
+        }
+
+        @Test
+        fun `should return 3 when a bag contains a bag that contains a bag that can contain given bag color`() {
+            // Given
+            val luggageProcessing = LuggageProcessing(
+                listOf(
+                    "bright white bags contain 1 shiny gold bag.",
+                    "dark orange bags contain 3 bright white bags, 4 muted yellow bags.",
+                    "dotted black bags contain 1 dark orange bag.",
+                )
+            )
+
+            // When
+            val numberOfBagsContainingColor = luggageProcessing.numberOfBagsContainingColor("shiny gold")
+
+            // Then
+            assertThat(numberOfBagsContainingColor).isEqualTo(3)
+        }
+
+        @Test
+        fun `DAY 7 - Part 1`() {
+            // Given
+            val luggageProcessing = LuggageProcessing(REAL_LUGGAGE_RULES)
+
+            // When
+            val numberOfBagsContainingColor = luggageProcessing.numberOfBagsContainingColor("shiny gold")
+
+            // Then
+            assertThat(numberOfBagsContainingColor).isEqualTo(197)
+        }
     }
 
-    @Test
-    fun `should return 1 when one bag can contain given bag color`() {
-        // Given
-        val luggageProcessing = LuggageProcessing(listOf("bright white bags contain 1 shiny gold bag."))
+    @Nested
+    inner class NumberOfBagsContained {
+        @Test
+        fun `should return 0 when given bag color contains no bag`() {
+            // Given
+            val luggageProcessing = LuggageProcessing(listOf("shiny gold bags contain no other bags."))
 
-        // When
-        val numberOfBagsContainingColor = luggageProcessing.numberOfBagsContainingColor("shiny gold")
+            // When
+            val numberOfBagsContainedBy = luggageProcessing.numberOfBagsContainedBy("shiny gold")
 
-        // Then
-        assertThat(numberOfBagsContainingColor).isEqualTo(1)
-    }
+            // Then
+            assertThat(numberOfBagsContainedBy).isEqualTo(0)
+        }
 
-    @Test
-    fun `should return 2  when a bag contains a bag that can contain given bag color`() {
-        // Given
-        val luggageProcessing = LuggageProcessing(listOf("bright white bags contain 1 shiny gold bag.", "dark orange bags contain 3 bright white bags, 4 muted yellow bags."))
+        @Test
+        fun `should count number of bags contained`() {
+            // Given
+            val luggageProcessing = LuggageProcessing(listOf("shiny gold bags contain 2 muted yellow bags."))
 
-        // When
-        val numberOfBagsContainingColor = luggageProcessing.numberOfBagsContainingColor("shiny gold")
+            // When
+            val numberOfBagsContainedBy = luggageProcessing.numberOfBagsContainedBy("shiny gold")
 
-        // Then
-        assertThat(numberOfBagsContainingColor).isEqualTo(2)
-    }
+            // Then
+            assertThat(numberOfBagsContainedBy).isEqualTo(2)
+        }
 
-    @Test
-    fun `should return 3 when a bag contains a bag that contains a bag that can contain given bag color`() {
-        // Given
-        val luggageProcessing = LuggageProcessing(listOf("bright white bags contain 1 shiny gold bag.",
-            "dark orange bags contain 3 bright white bags, 4 muted yellow bags.",
-            "dotted black bags contain 1 dark orange bag.",
-        ))
+        @Test
+        fun `should should also count number of bags contained by contained bags`() {
+            // Given
+            val luggageProcessing = LuggageProcessing(
+                listOf(
+                    "shiny gold bags contain 1 bright white bag.",
+                    "bright white bags contain 2 muted yellow bags."
+                )
+            )
 
-        // When
-        val numberOfBagsContainingColor = luggageProcessing.numberOfBagsContainingColor("shiny gold")
+            // When
+            val numberOfBagsContainedBy = luggageProcessing.numberOfBagsContainedBy("shiny gold")
 
-        // Then
-        assertThat(numberOfBagsContainingColor).isEqualTo(3)
-    }
+            // Then
+            assertThat(numberOfBagsContainedBy).isEqualTo(3)
+        }
 
-    @Test
-    fun `DAY 7 - Part 1`() {
-        // Given
-        val luggageProcessing = LuggageProcessing(REAL_LUGGAGE_RULES)
+        @Test
+        fun `should count bags contained by all contained bags`() {
+            // Given
+            val luggageProcessing = LuggageProcessing(
+                listOf(
+                    "shiny gold bags contain 1 bright white bag, 1 dark orange bag.",
+                    "bright white bags contain 2 muted yellow bags.",
+                    "dark orange bags contain 1 muted yellow bags."
+                )
+            )
 
-        // When
-        val numberOfBagsContainingColor = luggageProcessing.numberOfBagsContainingColor("shiny gold")
+            // When
+            val numberOfBagsContainedBy = luggageProcessing.numberOfBagsContainedBy("shiny gold")
 
-        // Then
-        assertThat(numberOfBagsContainingColor).isEqualTo(197)
+            // Then
+            assertThat(numberOfBagsContainedBy).isEqualTo(5)
+        }
+
+        @Test
+        fun `should multiply bags in contained bags by number of times this bag is contained`() {
+            // Given
+            val luggageProcessing = LuggageProcessing(
+                listOf(
+                    "shiny gold bags contain 2 bright white bag, 1 dark orange bag.",
+                    "bright white bags contain 2 muted yellow bags.",
+                    "dark orange bags contain 1 muted yellow bags."
+                )
+            )
+
+            // When
+            val numberOfBagsContainedBy = luggageProcessing.numberOfBagsContainedBy("shiny gold")
+
+            // Then
+            assertThat(numberOfBagsContainedBy).isEqualTo(8)
+        }
+
+        @Test
+        fun `DAY 7 - Part 2`() {
+            // Given
+            val luggageProcessing = LuggageProcessing(REAL_LUGGAGE_RULES)
+
+            // When
+            val numberOfBagsContainedBy = luggageProcessing.numberOfBagsContainedBy("shiny gold")
+
+            // Then
+            assertThat(numberOfBagsContainedBy).isEqualTo(85324)
+        }
     }
 }
