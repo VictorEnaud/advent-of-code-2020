9a8741fdaf8780e1a28416934b3b20a7850a610a
Victor Enaud <7222886+victorenaud@users.noreply.github.com>
2020-12-22 13:16:40 +0100

    Day 10: Part 2

--- src/main/java/Adapters.kt
+++ src/main/java/Adapters.kt
@@ -1,18 +1,51 @@
+import kotlin.math.pow
+
 class Adapters(textAdapters: List<String>) {
-    val adapters = textAdapters.map { textAdapter -> textAdapter.toInt() }.sorted()
+    private val adapters: List<Int>
+
+    init {
+        val convertedAdapters = textAdapters.map { textAdapter -> textAdapter.toInt() }
+        adapters = convertedAdapters.plus(0)
+            .plus(convertedAdapters.maxOrNull()!! + 3)
+            .sorted()
+    }
 
     fun oneAndThreeJoltsDifferenceMultiplication(): Int {
-        var oneJoltDifferences = 1
-        var threeJoltDifferences = 1
+        var oneJoltDifferences = 0
+        var threeJoltDifferences = 0
         adapters.reduce { previousAdapter, currentAdapter ->
             if (currentAdapter - previousAdapter == 1) {
                 oneJoltDifferences++
-            } else if (currentAdapter-previousAdapter==3) {
+            } else if (currentAdapter - previousAdapter == 3) {
                 threeJoltDifferences++
             }
             return@reduce currentAdapter
         }
 
         return oneJoltDifferences * threeJoltDifferences
+    }
+
+    fun possibleCombinations(): Long {
+        var possibleCombinations = 1L
+
+        val adaptersWithoutDevice = adapters.dropLast(1)
+        var index = 0
+        while (index in 0..adaptersWithoutDevice.size) {
+            val adapter = adaptersWithoutDevice[index]
+            val possibleAdapters = adapters.filter { it in adapter + 1..adapter + 3 }
+            var additionalCombinations = 2.toDouble().pow(possibleAdapters.count() - 1)
+            index=adaptersWithoutDevice.indexOf(possibleAdapters.last())
+            if (possibleAdapters.count() == 3) {
+                var i = 4
+                while (adapters.contains(adapter+i)) {
+                    additionalCombinations+=3
+                    index=adapters.indexOf(adapter+i)
+                    i++
+                }
+            }
+            possibleCombinations *= additionalCombinations.toLong()
+        }
+
+        return possibleCombinations
     }
 }
--- src/test/java/AdaptersTest.kt
+++ src/test/java/AdaptersTest.kt
@@ -64,4 +64,185 @@ internal class AdaptersTest {
         // Then
         assertThat(oneAndThreeJoltsDifferenceMultiplication).isEqualTo(1984)
     }
+
+    @Test
+    fun `should have only one possible combination when only one number`() {
+        // Given
+        val adapters = Adapters(listOf("1"))
+
+        // When
+        val possibleCombinations = adapters.possibleCombinations()
+
+        // Then
+        assertThat(possibleCombinations).isEqualTo(1)
+    }
+
+    @Test
+    fun `should have only one possible combination when all adapters are separated by 3 jolts`() {
+        // Given
+        val adapters = Adapters(listOf("1", "4", "7"))
+
+        // When
+        val possibleCombinations = adapters.possibleCombinations()
+
+        // Then
+        assertThat(possibleCombinations).isEqualTo(1)
+    }
+
+    @Test
+    fun `should have two possible combinations`() {
+        // Given
+        val adapters = Adapters(listOf("1", "4", "5", "7"))
+
+        // When
+        val possibleCombinations = adapters.possibleCombinations()
+
+        // Then
+        assertThat(possibleCombinations).isEqualTo(2)
+    }
+
+    @Test
+    fun `should have four possible combinations`() {
+        // Given
+        val adapters = Adapters(listOf("1", "4", "5", "6", "7"))
+        // 0 1 4 5 6 7 10
+        // 0 1 4 5 7 10
+        // 0 1 4 6 7 10
+        // 0 1 4 7 10
+
+        // When
+        val possibleCombinations = adapters.possibleCombinations()
+
+        // Then
+        assertThat(possibleCombinations).isEqualTo(4)
+    }
+
+    @Test
+    fun `should have not ignore 5 consecutive numbers`() {
+        // Given
+        val adapters = Adapters(listOf("1", "4", "5", "6", "7", "8"))
+        // 0 1 4 5 6 7 8 11
+        // 0 1 4 5 6 8 11 NEW
+        // 0 1 4 5 7 8 11
+        // 0 1 4 5 8 11 NEW
+        // 0 1 4 6 7 8 11
+        // 0 1 4 6 8 11 NEW
+        // 0 1 4 7 8 11
+
+        // When
+        val possibleCombinations = adapters.possibleCombinations()
+
+        // Then
+        assertThat(possibleCombinations).isEqualTo(7)
+    }
+
+    @Test
+    fun `should have not ignore 6 consecutive numbers`() {
+        // Given
+        val adapters = Adapters(listOf("1", "4", "5", "6", "7", "8", "9"))
+        // 0 1 4 5 6 7 8 9 12
+        // 0 1 4 5 6 8 9 12
+        // 0 1 4 5 7 8 9 12
+        // 0 1 4 5 8 9 12
+        // 0 1 4 6 7 8 9 12
+        // 0 1 4 6 7 9 12 NEW
+        // 0 1 4 6 8 9 12
+        // 0 1 4 6 9 12 NEW
+        // 0 1 4 7 8 9 12
+        // 0 1 4 7 9 12 NEW
+
+        // When
+        val possibleCombinations = adapters.possibleCombinations()
+
+        // Then
+        assertThat(possibleCombinations).isEqualTo(10)
+    }
+
+    @Test
+    fun `should multiply possible combinations`() {
+        // Given
+        val adapters = Adapters(listOf("1", "4", "5", "6", "9", "10", "11"))
+
+        // When
+        val possibleCombinations = adapters.possibleCombinations()
+
+        // Then
+        assertThat(possibleCombinations).isEqualTo(4)
+    }
+
+    @Test
+    fun `should have eight possible combinations`() {
+        // Given
+        val adapters = Adapters(listOf("16",
+                "10",
+                "15",
+                "5",
+                "1",
+                "11",
+                "7",
+                "19",
+                "6",
+                "12",
+                "4"))
+
+        // When
+        val possibleCombinations = adapters.possibleCombinations()
+
+        // Then
+        assertThat(possibleCombinations).isEqualTo(8)
+    }
+
+    @Test
+    fun `should have 19208 possible combinations`() {
+        // Given
+        val adapters = Adapters(listOf("28",
+                "33",
+                "18",
+                "42",
+                "31",
+                "14",
+                "46",
+                "20",
+                "48",
+                "47",
+                "24",
+                "23",
+                "49",
+                "45",
+                "19",
+                "38",
+                "39",
+                "11",
+                "1",
+                "32",
+                "25",
+                "35",
+                "8",
+                "17",
+                "7",
+                "9",
+                "4",
+                "2",
+                "34",
+                "10",
+                "3"))
+
+        // When
+        val possibleCombinations = adapters.possibleCombinations()
+
+        // Then
+        assertThat(possibleCombinations).isEqualTo(19208)
+    }
+
+    @Test
+    fun `Day 10 - Part 2`() {
+        // Given
+        val adapters = Adapters(REAL_ADAPTERS_INPUT)
+
+        // When
+        val possibleCombinations = adapters.possibleCombinations()
+
+        // Then
+        assertThat(possibleCombinations).isEqualTo(3543369523456)
+    }
 }
